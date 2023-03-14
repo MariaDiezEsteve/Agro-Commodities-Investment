6b87ccf532973efacc7aa77955936813b6867836
MariaDiezEsteve <diezesteve@outlook.es>
2023-03-14 14:26:33 +0100

    incluido scss para el proyecto y bootstrap, preparado para estilos

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router, bootstrap).mount('#app')
