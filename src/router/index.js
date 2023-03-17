35dac95c569785b49b0ab68252db2fdf384e17a9
MariaDiezEsteve <diezesteve@outlook.es>
2023-03-17 14:17:07 +0100

    estructura de carpetas creadas y sass iniciado

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'HomeView',
+    component: () => import('../views/HomeView.vue')
   },
   {
     path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    name: 'AboutUsView',
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/details',
+    name: 'Details',
+    component: () => import('../views/DetailsView.vue')
+  },
+  {
+    path: '/user',
+    name: 'User',
+    component: () => import('../views/UserView.vue')
+  },
+  {
+    path: '/blog',
+    name: 'Blog',
+    component: () => import('../views/BlogView.vue')
   }
+
 ]
 
 const router = createRouter({
---
+++ src/views/DetailsView.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1></h1>
+  </template>
+  
+  <script setup>
+
+  </script>
+  
+  <style lang="sass" scoped>
+  
+  </style>
