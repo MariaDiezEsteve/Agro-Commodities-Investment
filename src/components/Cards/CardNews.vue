1c421f92f3f9b0f83d324242c2fbc33d3ca774d9
Lucasibf5 <ibarlucas@gmail.com>
2023-03-20 13:28:06 +0100

    card sin foto de maiz lucas

--- src/components/Cards/CardNews.vue
+++ src/components/Cards/CardNews.vue
@@ -2,15 +2,15 @@
     <div class="card mb-3" style="max-width: 540px;">
   <div class="row g-0">
     <div class="col-md-4">
-      <img src="cafe.jpg" class="img-fluid rounded-start" alt="cafe">
+      <img src="src/assets/images/imagenes-blog/trigo.jpg">
     </div>
     <div class="col-md-8">
       <div class="card-body">
         <h5 class="card-title"></h5>
-        <p class="Es importante recordar cada año que se deben cumplir las normas obligatorias y seguir todas las buenas prácticas en el manejo del cultivo.
+        <p class="card-text">Es importante recordar cada año que se deben cumplir las normas obligatorias y seguir todas las buenas prácticas en el manejo del cultivo.
 
-Hay que tener en cuenta que se debe sembrar el 20% de la superficie como refugio, que es obligatorio hacerlo para que, en el caso de que aparecieran taladros resistentes al maíz Bt, lo más probable es que se aparearan con taladros no resistentes del refugio, y conseguir así que las siguientes generaciones se pudieran controlar con el maíz Bt. Se ha hecho así desde hace 25 años y gracias a ello, hasta el momento nunca han aparecido insectos resistentes."></p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+Hay que tener en cuenta que se debe sembrar el 20% de la superficie como refugio, que es obligatorio hacerlo para que, en el caso de que aparecieran taladros resistentes al maíz Bt...</p>
+        <p class="card-text"><small class="text-muted">15/3/2023</small></p>
       </div>
     </div>
 
