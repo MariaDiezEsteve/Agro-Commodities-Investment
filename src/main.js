857e63747833ad12d8632edf35f757b191f1d224
Zguedest <zucelys.zge@gmail.com>
2023-03-20 01:38:25 +0100

    importando bootstrap sin problemas

--- src/main.js
+++ src/main.js
@@ -2,13 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-/* La parte de Boostrap da errores por lo que se ha comentado*/
+/* debes poner en tu consola npm i bootstrap y no te dara error*/
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.js'
 
-// import * as bootstrap from 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// createApp(App).use(router, bootstrap).mount('#app')
-
-createApp(App).use(router).mount('#app')
+createApp(App).use(router,bootstrap).mount('#app')
 
 // QT70XFV8RWVYA560  //esto que es??
 
