973c15cc9e01ede76e4aa06d1086fe391bd67207
Lucasibf5 <ibarlucas@gmail.com>
2023-03-20 12:00:55 +0100

    creando  creando 1  tarjeta blog

--- src/components/Cards/CardNews.vue
+++ src/components/Cards/CardNews.vue
@@ -2,15 +2,18 @@
     <div class="card mb-3" style="max-width: 540px;">
   <div class="row g-0">
     <div class="col-md-4">
-      <img src="" class="img-fluid rounded-start" alt="...">
+      <img src="cafe.jpg" class="img-fluid rounded-start" alt="cafe">
     </div>
     <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h5 class="card-title"></h5>
+        <p class="card-text"></p>
         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
       </div>
     </div>
+
+
+    
   </div>
 </div>
   </template>
