ca31064e35687d3bd2a3c02a672e2ce4e42e7d34
Zguedest <zucelys.zge@gmail.com>
2023-03-20 01:29:12 +0100

    detalles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,15 +2,11 @@
 
 <template>
   <h1>{{titulo}}</h1>
-
   <img v-if="data.isError" src="@/assets/images/error.jpeg" alt="error">
-  <button @click="read">Mostrar Datos</button>
   <img v-if="isLoading" src="@/assets/images/spin.gif" alt="loading">
   <div v-if="!data.isError && !data.isLoading" >
-    <!-- <h1>{{titulo}}</h1> -->
     <h2>Loading es: {{isLoading}}</h2>
     <h2>Error es: {{data.isError}}</h2>
-
     <h2>{{data.prodts}}</h2>
   </div> 
 
@@ -18,8 +14,6 @@
 
 
 <script setup>
-//import { Suspense } from 'vue';
-
 import info from '@/logic/dataInfo'
 import {ref,onMounted} from "vue";
 
@@ -35,8 +29,8 @@ let data = ref(onMounted(async () => {
   
 </script>
 
-<style lang="css" scoped> puse css porque sass esta dando error luego reviso
-
+<style lang="css" scoped> 
+/* puse css porque sass esta dando error luego reviso */
   h1{
     color:blue;
   }
