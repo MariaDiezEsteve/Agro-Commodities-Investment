12c45dda8413ec0f96b165e9c8af7bdbcff7c4a8
Zguedest <zucelys.zge@gmail.com>
2023-03-20 01:17:23 +0100

    axios con gestion de errores

--- src/logic/dataInfo.js
+++ src/logic/dataInfo.js
@@ -1,45 +1,50 @@
 import axios from 'axios'
 import {ref} from 'vue'
 
-let isError = false
-let isLoading = true
-
-const prodts= {
-  wheat : ref(null),
-  sugar : ref(null),
-  cotton : ref(null),
-  coffee : ref(null),
-  corn : ref(null),
-
-}
-
-
-const url={
-  wheat: "https:www.alphavantage.co/query?function=WHEAT&interval=monthly&apikey=demo",
-  sugar: "https://www.alphavantage.co/query?function=SUGAR&interval=monthly&apikey=demo",
-  corn: "https://www.alphavantage.co/query?function=CORN&interval=monthly&apikey=demo",
-  cotton: "https://www.alphavantage.co/query?function=COTTON&interval=monthly&apikey=demo",
-  coffee: "https://www.alphavantage.co/query?function=COFFEE&interval=monthly&apikey=demo"
-}
-
-
 async function getData(){
-    for (let clave in url){
 
+    let isError = false
+    let isLoading = true 
+
+    const prodts= {
+        wheat : ref(null), //por que poner aqui ref?
+        sugar : ref(null),
+        cotton : ref(null),
+        coffee : ref(null),
+        corn : ref(null),
+      
+      }
+      
+      
+      const url={
+        wheat: "https:www.alphavantage.co/query?function=WHEAT&interval=monthly&apikey=demo",
+        sugar: "https://www.alphavantage.co/query?function=SUGAR&interval=monthly&apikey=demo",
+        corn: "https://www.alphavantage.co/query?function=CORN&interval=monthly&apikey=demo",
+        cotton: "https://www.alphavantage.co/query?function=COTTON&interval=monthly&apikey=demo",
+        coffee: "https://www.alphavantage.co/query?function=COFFEE&interval=monthly&apikey=demo"
+      }
+    
+    for (let clave in url){
         try{
+            console.log()
             let response = await axios.get(url[clave]);
-            isLoading = false
-            prodts[clave]=response.data
-            console.log("aa",clave,prodts[clave])
+            prodts[clave] = response.data
+            //console.log("data",clave,prodts[clave])
         }catch (error) {
             console.log(error);
             isError = true
         }
     }
- }
+    isLoading = false
+
+    return{
+        isError, isLoading, prodts, url
+    }
+     
+  }
 
 
-getData()
+export default {getData} 
 
-export default {prodts, url, isError, isLoading} 
+
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,25 +2,37 @@
 
 <template>
   <h1>{{titulo}}</h1>
-  <h2>{{data}}</h2> <!-- se muestra el arreglo de objetos que contiene el objeto prodts y el objeto url -->
-  <h2>{{data.prodts}}</h2> <!-- se muestra solo el objeto de prodts-->
+
+  <img v-if="data.isError" src="@/assets/images/error.jpeg" alt="error">
+  <button @click="read">Mostrar Datos</button>
+  <img v-if="isLoading" src="@/assets/images/spin.gif" alt="loading">
+  <div v-if="!data.isError && !data.isLoading" >
+    <!-- <h1>{{titulo}}</h1> -->
+    <h2>Loading es: {{isLoading}}</h2>
+    <h2>Error es: {{data.isError}}</h2>
+
+    <h2>{{data.prodts}}</h2>
+  </div> 
 
 </template>
 
 
 <script setup>
-// al poner import algo se crea un objeto con ese nombre que contiene
-// todo lo que este en el export defaul del js 
-  import data from '@/logic/dataInfo' 
+//import { Suspense } from 'vue';
 
-  let titulo="prueba"
+import info from '@/logic/dataInfo'
+import {ref,onMounted} from "vue";
 
-  for (let clave in data.prodts){
+let titulo="prueba" 
+let isLoading = ref(true) 
 
-    console.log("aa",clave,data.prodts[clave])
+let data = ref(onMounted(async () => {
+  data.value = await info.getData()
+  if( !data.value.isLoading){
+    isLoading.value = false
   }
-
-     
+})) 
+  
 </script>
 
 <style lang="css" scoped> puse css porque sass esta dando error luego reviso
