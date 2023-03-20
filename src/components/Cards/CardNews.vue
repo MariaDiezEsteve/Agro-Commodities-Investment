1eee70698ceba8e2af46c280ed5fa707ab72dd6b
Lucasibf5 <ibarlucas@gmail.com>
2023-03-20 12:40:43 +0100

    lucas primer card blog

--- src/components/Cards/CardNews.vue
+++ src/components/Cards/CardNews.vue
@@ -7,7 +7,9 @@
     <div class="col-md-8">
       <div class="card-body">
         <h5 class="card-title"></h5>
-        <p class="card-text"></p>
+        <p class="Es importante recordar cada año que se deben cumplir las normas obligatorias y seguir todas las buenas prácticas en el manejo del cultivo.
+
+Hay que tener en cuenta que se debe sembrar el 20% de la superficie como refugio, que es obligatorio hacerlo para que, en el caso de que aparecieran taladros resistentes al maíz Bt, lo más probable es que se aparearan con taladros no resistentes del refugio, y conseguir así que las siguientes generaciones se pudieran controlar con el maíz Bt. Se ha hecho así desde hace 25 años y gracias a ello, hasta el momento nunca han aparecido insectos resistentes."></p>
         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
       </div>
     </div>
