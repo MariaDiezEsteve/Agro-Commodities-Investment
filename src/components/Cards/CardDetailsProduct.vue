1ad10b4256025a7b53864b54b104fae600dc1acf
JJessa <jessicamosteiro13@gmail.com>
2023-03-21 14:31:48 +0100

    incluido los datos de las cards donde se ven los valores. Problemas para utilizar los props en setup script

--- src/components/Cards/CardDetailsProduct.vue
+++ src/components/Cards/CardDetailsProduct.vue
@@ -1,11 +1,48 @@
 <template>
-    <h1></h1>
+  <!-- <h1>{{ data.prodts}}</h1>  -->
+    
+  <div class="card" style="width: 18rem;">
+    <!-- <div v-for="prodt in data.prodts" :key="prodt.name" class="card-body">
+      <h5 class="card-title">{{ (prodt.name).slice(15)}}</h5>
+      <div class="card-details">
+        <p class="card-text">{{ parseFloat(prodt.data[0].value).toFixed(2) }}</p>
+        <p class="card-text">{{ parseFloat(((prodt.data[0].value)-(prodt.data[1].value)) / (prodt.data[1].value)).toFixed(3) }} %</p>
+      </div>
+    </div> -->
+  </div>
+
   </template>
   
   <script setup>
+  /* eslint-disable */
+    import { defineProps,ref,onMounted } from 'vue';
 
-</script>
-  
-  <style lang="sass" scoped>
+   
+    const props = defineProps({
+    data: Object
+    })
+
+
+
+
+    let dat = ref(onMounted(async () => {
+    dat.value = await props.data
+    
+    console.log("el prop pesao",dat.value)
+    })) 
+
+    
+    
+    
+    
+
+  </script>
+
+  <style scoped>
+
+  .card-details {
+    display: flex;
+    justify-content: space-around;
+  }
   
   </style>
--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -1,11 +1,28 @@
 <template>
-    <h1></h1>
+    <h1>Agro Commodities Details</h1>
+    <p>This page shows the information details about the following commodities:</p>
+    
+    <DetailsProducts :data="data"/>
+
   </template>
   
   <script setup>
+  import info from '@/logic/dataInfo'
+  import {ref,onMounted} from "vue";
+  import DetailsProducts from '@/components/Cards/CardDetailsProduct.vue'
 
+  let isLoading = ref(true) 
+
+  let data = ref(onMounted(async () => {
+    data.value = await info.getData()
+    
+    if( !data.value.isLoading){
+      isLoading.value = false
+    }
+  })) 
+   console.log(data.value)
   </script>
   
-  <style lang="scss" scoped>
+  <style scoped>
   
   </style>
