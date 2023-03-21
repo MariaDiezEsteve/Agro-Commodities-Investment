ac2471a716870bb48ef7c17eebc5dd0d75e494dd
Lucasibf5 <ibarlucas@gmail.com>
2023-03-21 09:41:45 +0100

    comenzar descripcion card lucas

--- src/components/Cards/CardNews.vue
+++ src/components/Cards/CardNews.vue
@@ -22,16 +22,35 @@ Hay que tener en cuenta que se debe sembrar el 20% de la superficie como refugio
 
   </template>
   
-  <script>
+  <script setup>
   
+  const blogProduct  = [
+  { 
+
+    id: 1,
+    title:"Cotton",
+    description:"When it comes to finding the best broker for CFD cotton trading, there are a few things to consider.Firstly, look at the fees and commissions that brokers charge. Different brokers will offer different fees and it is important to compare different brokers in order to find one that suits your budget. Secondly, check the trading platform offered. It is important to make sure that the platform works well and has all the features you need. Finally, look at the customer service. A good broker should be able to provide fast and helpful customer service in case you have any questions or need help with your order",
+    src:
+
+
+
+
+  }
+
+  ]
   
-  export default {
-    props: {
-      imagen: String,
-      titulo: String,
-      descripcion:String,
-      enlace: String,
-    },
+
+
+
+
+
+  //export default {
+    // props: {
+    //   imagen: String,
+    //   titulo: String,
+    //   descripcion:String,
+    //   enlace: String,
+    // },
 
     // setup(props) {
     //  const imagen = []
