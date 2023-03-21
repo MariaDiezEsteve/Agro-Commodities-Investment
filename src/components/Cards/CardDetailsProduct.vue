c0b8908a79ec7192cf1c6977fb79c09684562c98
Zguedest <zucelys.zge@gmail.com>
2023-03-21 20:23:11 +0100

    arreglando problema de prop

--- src/components/Cards/CardDetailsProduct.vue
+++ src/components/Cards/CardDetailsProduct.vue
@@ -1,48 +1,68 @@
 <template>
-  <!-- <h1>{{ data.prodts}}</h1>  -->
+  <!-- <h1>{{props.data.prodts}}</h1>  -->
     
   <div class="card" style="width: 18rem;">
-    <!-- <div v-for="prodt in data.prodts" :key="prodt.name" class="card-body">
+    <div v-for="prodt in data.prodts" :key="prodt.name" class="card-body">
+      {{ calcularPorcentaje(prodt) }}
       <h5 class="card-title">{{ (prodt.name).slice(15)}}</h5>
       <div class="card-details">
         <p class="card-text">{{ parseFloat(prodt.data[0].value).toFixed(2) }}</p>
-        <p class="card-text">{{ parseFloat(((prodt.data[0].value)-(prodt.data[1].value)) / (prodt.data[1].value)).toFixed(3) }} %</p>
+        <p :class='classPorcentaje'>{{ porcentaje }} %</p>
       </div>
-    </div> -->
+    </div>
   </div>
 
   </template>
   
   <script setup>
-  /* eslint-disable */
-    import { defineProps,ref,onMounted } from 'vue';
+    import {defineProps,ref} from 'vue';
+    import { computed } from "@vue/reactivity"
 
-   
-    const props = defineProps({
-    data: Object
+    defineProps({
+      data: Object
     })
 
+    const porcentaje = ref (0) 
 
-
-
-    let dat = ref(onMounted(async () => {
-    dat.value = await props.data
-    
-    console.log("el prop pesao",dat.value)
-    })) 
+    function calcularPorcentaje(prodt){
+      porcentaje.value = parseFloat(((prodt.data[0].value)-(prodt.data[1].value)) / (prodt.data[1].value)).toFixed(3)
+    }
 
     
-    
-    
-    
+    const classPorcentaje = computed(() => {
+      let result = '';
+      if (porcentaje.value === 0) {
+          result = 'zero';
+      } else if (porcentaje.value > 0) {
+          result = 'positive';
+      } else {
+          result = 'negative';
+      }
+      return result;
+    });
+
 
   </script>
 
-  <style scoped>
+  <style lang="scss" scoped>
+    @import "@/assets/Sass/--parcial.scss";
+
 
   .card-details {
     display: flex;
     justify-content: space-around;
+  }
+
+  .positive {
+    color: $green;
+    }
+
+  .negative {
+  color:$red;
+  }
+
+  .zero {
+  color: $blueDark;
   }
   
   </style>
--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -1,8 +1,12 @@
 <template>
     <h1>Agro Commodities Details</h1>
     <p>This page shows the information details about the following commodities:</p>
+    <img v-if="data.isError" src="@/assets/images/error.jpeg" alt="error">
+    <img v-if="isLoading" src="@/assets/images/spin.gif" alt="loading">
+    <div v-if="!data.isError && !isLoading" >
+      <DetailsProducts :data = "data"/>
+    </div>
     
-    <DetailsProducts :data="data"/>
 
   </template>
   
@@ -12,17 +16,18 @@
   import DetailsProducts from '@/components/Cards/CardDetailsProduct.vue'
 
   let isLoading = ref(true) 
-
-  let data = ref(onMounted(async () => {
+  let data =  ref(onMounted(async () => {
     data.value = await info.getData()
-    
     if( !data.value.isLoading){
       isLoading.value = false
     }
-  })) 
-   console.log(data.value)
+
+  }))
+  
   </script>
   
-  <style scoped>
+  <style lang="scss" scoped>
+    @import "@/assets/Sass/--parcial.scss";
+
   
   </style>
