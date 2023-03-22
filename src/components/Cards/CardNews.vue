302852c853bc14fe34b6abc2d5578418b85ebd71
nasre21 <nasou2001@gmail.com>
2023-03-22 09:55:32 +0100

    terminamos estilos de las tarjetas

--- src/components/Cards/CardNews.vue
+++ src/components/Cards/CardNews.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="card mb-3" v-for ="blogProduct in blogProducts" :key="blogProduct.id">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="@/assets/images/imagenes-blog/cafe.jpg" class="img-fluid rounded-start" alt="blogProduct.alt">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">{{ blogProduct.title }}</h5>
-        <p class="card-text">{{ blogProduct.description }}</p>
-        <button><router-link :to = "blogProduct.enlace">More information</router-link></button>
-        <p class="card-text"><small class="text-muted">{{ blogProduct.date }}</small></p>
+  <div class="card d-block m-auto mb-3"
+    v-for="blogProduct in blogProducts"
+    :key="blogProduct.id"
+  >
+    <div class="row d-flex justify-content-center g-0">
+      <div class="col-md-4">
+        <img
+          src="@/assets/images/imagenes-blog/cotton.jpg"
+          class="img-fluid rounded-start"
+          alt="blogProduct.alt"
+        />
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">{{ blogProduct.title }}</h5>
+          <p class="card-text">{{ blogProduct.description }}</p>
+          <button >
+            <router-link class="btn-router" :to="blogProduct.enlace">More information</router-link>
+          </button>
+          <p class="card-text px-5">
+            <small class="text-muted">{{ blogProduct.date }}</small>
+          </p>
+        </div>
       </div>
     </div>
-  </div>
   </div>
 
   <!-- <div class="card" v-for ="blogProduct in blogProducts" :key="blogProduct.id">
@@ -23,48 +34,47 @@
         <button><router-link :to = "blogProduct.enlace">More information</router-link></button>
       </div>
     </div> -->
-
 </template>
-  
-  <script setup>
-  
-  const blogProducts  = [
-  { 
 
+<script setup>
+const blogProducts = [
+  {
     id: 1,
-    title:"Cotton",
-    description:"When it comes to finding the best broker for CFD cotton trading, there are a few things to consider.Firstly, look at the fees and commissions that brokers charge. Different brokers will offer different fees and it is important to compare different brokers in order to find one that suits your budget. Secondly, check the trading platform offered. It is important to make sure that the platform works well and has all the features you need.",
+    title: "Cotton",
+    description:
+      "When it comes to finding the best broker for CFD cotton trading, there are a few things to consider.Firstly, look at the fees and commissions that brokers charge. Different brokers will offer different fees and it is important to compare different brokers in order to find one that suits your budget... ",
     src: "@/assets/images/imagenes-blog/algodon.jpg",
     alt: "imagen algodón",
-    enlace: '../views/DetailsView.vue',
-    date:"March 2023",
-  }
-  ]
-  
+    enlace: "../views/DetailsView.vue",
+    date: "March 2023",
+  },
+];
+</script>
 
+<style lang="scss" scoped>
+@import "@/assets/Sass/--parcial.scss";
 
- </script>
-  
-  
-  <style lang="scss" scoped>
- @import "@/assets/Sass/--parcial.scss";
-
-
- .card{
+.card {
   display: flex;
   justify-content: center;
   width: 65%;
-      img{
-
+  
+  img {
     height: 100%;
-    }
- }
-
- 
-
-
-  button{
-    @include button ($bg-color:$blueDark, $wth: 14rem, $colorletra:$white_color);
   }
- 
-  </style>
+}
+.card-text{
+  padding-left:2.5rem;
+  padding-right:2.5rem;
+}
+.btn-router{
+  color: white;
+  text-decoration: none;
+  padding-bottom: 1rem;
+}
+
+button {
+  @include button($bg-color: $blueDark, $wth: 14rem, $colorletra: $white_color);
+  font-size: 0;
+}
+</style>
