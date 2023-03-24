9163b1df192d2572d0d574dd8ab15d2536d458c6
Zguedest <zucelys.zge@gmail.com>
2023-03-24 01:22:09 +0100

    componente BarChart

--- src/components/Charts/BarChart.vue
+++ src/components/Charts/BarChart.vue
@@ -1,48 +1,92 @@
 <template>
-  <div>
-    <canvas id="myChart"></canvas>
+  <div class="d-flex justify-content-center pt-4">
+    <div style="width: 60%;">
+      <canvas id="myChart"></canvas>
+    </div>
+
   </div>
+  
 </template>
   
 <script setup>
+
   import {defineProps,onMounted} from 'vue';
   import Chart from 'chart.js/auto'; //npm install chart.js
 
-  const props = defineProps({
+  const prop= defineProps({
     data: Object
   })
 
-  let myChart;
-  onMounted(()=>{ //muy importante el onMounted para coger cosas del template es aqui dentro
-    const ctx = document.getElementById("myChart")
-    console.log("esto",ctx)
-    const labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-    const dataProdt = {
-      labels: labels,
-      datasets: [{
-        label: "Monthly Price",
-        data: [props.data.prodts.wheat.data.value],
-        backgroundColor: ['rgb(248, 239, 11)'],
-      }],
-    }
+  onMounted(()=>{//muy importante el onMounted para coger cosas del template es aqui dentro
+    graph()
+  });
 
+
+  let graph = () => { //esta funcion se dinamiza con un prop del producto seleccionado
+     let option = "years" //esto seria otro prop
+
+     let optChart = "bar" //esto puede ser un drop y el usuario elegir como quiere ver el chart
+
+    let myChart;
+    const ctx = document.getElementById('myChart')
+    const  months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    const years = [2000, 2001, 2002, 2003, 2004,2005, 2006] // hacer funcion q dado 2 años devuelva un arreglo con el rango de años
+    console.log("precios dataaaa", prop.data.prodts.wheat.data)
+      
+    const data = {
+      labels: option === "months" ? months : years,
+      datasets: [{
+        label: 'Wheat Price',  // porp del producto seleccionado + Price
+        data: option === "months" ? pricesPerMonthInAYear(2000) : averagePricesByYearRange (2000,2006) , //coger el año de la interfaz
+        fill: true,
+        borderColor: 'rgb(75, 192, 192)',
+        backgroundColor:'rgba(248, 238, 11,0.7)',
+        tension: 0,
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+        }
+      }
+    ]
+    }
+    const chartWithKey = Chart.getChart('myChart')
+    if (chartWithKey != undefined) {
+      chartWithKey.destroy()
+    }
     myChart = new Chart(ctx, {
-      type: "line",
-      data: dataProdt
+      type: optChart,
+      data: data,
     })
 
-    console.log(myChart)
+    return myChart
+  }
 
-  }) ;
+
+  let pricesPerMonthInAYear = (year)=>{
+    let productData = prop.data.prodts.wheat.data
+    productData = productData.filter(element => parseInt((element.date).slice(0,4)) == year)
+    productData = productData.map(element =>  parseFloat((element.value)).toFixed(2));
+    return productData
+  }
+
+  let averagePricesByYearRange = (year1,year2)=>{
+    let productData = prop.data.prodts.wheat.data
+    let sum=0,productDataYear=[], prices=[], avgs =[]
+    while (year1<(year2+1)) {
+      productDataYear = productData.filter(element => parseInt((element.date).slice(0,4)) == year1)  
+      prices = productDataYear.map(element =>  parseFloat((element.value)).toFixed(2));
+      sum = 0
+      for( let i=0; i<prices.length ; i++){
+        sum = sum + parseFloat(prices[i])
+      } 
+      avgs.push(sum/prices.length)
+      year1++
+    }
+    return avgs
+  }
   
-
-  
-
-  //console.log(props.data.prodts)
 </script>
   
-  <style lang="scss" scoped>
-    @import "@/assets/Sass/--parcial.scss";
-
-  
-  </style>
+<style lang="scss" scoped>
+  @import "@/assets/Sass/--parcial.scss";
+</style>
