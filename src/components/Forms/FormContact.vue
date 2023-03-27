5bf73d451f99e4b496de31df82a53f4859db0297
Lucasibf5 <ibarlucas@gmail.com>
2023-03-27 10:00:22 +0200

    COMENZAR CONTAC FormContact

--- src/components/Forms/FormContact.vue
+++ src/components/Forms/FormContact.vue
@@ -1,5 +1,18 @@
 <template>
     <h1>Contact Form</h1>
+     <p>If you want to contact us you can do it with this contact form</p>
+
+     
+ FORMULARIO
+
+     <div class="row">
+  <div class="col">
+    <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+  </div>
+  <div class="col">
+    <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+  </div>
+</div>
   </template>
   
   <script setup>
--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -1,11 +1,12 @@
 <template>
   <h1>About Us</h1>
    <SideBar />
-   <FormContat />
+   <FormContact/>
 </template>
 
 <script setup>
 import SideBar from '@/components/Commons/SideBar.vue'
+import FormContact from '@/components/Forms/FormContact.vue';
 
 </script>
 
