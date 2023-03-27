1c3b8c805b3f47d13491f86a100e2c784e54a1ff
Lucasibf5 <ibarlucas@gmail.com>
2023-03-27 13:37:24 +0200

    ww

--- src/components/Forms/FormContact.vue
+++ src/components/Forms/FormContact.vue
@@ -1,62 +1,79 @@
 <template>
-   <div v-for="FormContact in formContact"
-   :key="FormContact.id">
- 
- <!-- FORMULARIO -->
-
- <div>
-  <div class="card-body">
-    <h5 class="card-title">Contact Form</h5>
-    <p class="card-text">If you want to contact us you can do it with this contact form</p>
-  </div>
-
-    <div class="row">
-  <div class="col">
-    <input type="text" class="form-control" placeholder="Enter your name" aria-label="Enter your name">
-  </div>
-  <div class="col">
-    <input type="text" class="form-control" placeholder="Enter your email address" aria-label="Enter your email address">
-  </div>
-
-  <div class="mb-3">
-  <label for="exampleFormControlTextarea1"    class="form-label"></label>
-    <textarea class="form-control" placeholder="Type here your message" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div>
-
-
-    <a href="#" class="btn btn-primary">Send Message</a>
-
-  </div>
-  </div>
-</div>
-
-  </template>
   
+    <div class="form-card m-4">
+      <div class="card-body">
+        <h5 class="card-title">Contact Form</h5>
+        <p class="card-text">
+          If you want to contact us you can do it with this contact form
+        </p>
+      </div>
 
+      <div class="row">
+        <div class="col">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Enter your name"
+          />
+        </div>
+        <div class="col">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Enter your email address"
+          />
+        </div>
 
-<script setup>
+        <div class="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label"></label>
+          <textarea
+            class="form-control"
+            placeholder="Type here your message"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
+        </div>
+
+        <div class="buttones">
+          <a href="#" class="btn btn-primary">Send Message</a>
+        </div>
+      </div>
+    </div>
+ 
+  <!-- FORMULARIO -->
+</template>
+
+<!-- <script setup>
 const formContact = [
   {
     id: 1,
-    title:"Contact Form",
-    name:"",
-    email:"",
-    text:"",
+    title: "Contact Form",
+    name: "",
+    email: "",
+    text: "",
     button: "Send Message",
   },
- 
 ];
+</script> -->
 
-  </script>
-  
-  <style lang="scss" scoped>
-  @import "@/assets/Sass/--parcial.scss";
+<style lang="scss" scoped>
+@import "@/assets/Sass/--parcial.scss";
 
-  
+.buttones {
+  a {
+    @include button(
+      $bg-color: $blueDark,
+      $wth: 11rem,
+      $colorletra: $white_color
+    );
+  }
+}
+
+.form-card{
 
 
- 
 
-  
-  
-  </style>
+
+}
+
+</style>
