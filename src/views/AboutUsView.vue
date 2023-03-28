9c33dce4aa91fea5fa9ddcab11910d6a17939b6d
nasre21 <nasou2001@gmail.com>
2023-03-28 08:57:23 +0200

    resolvel cambios

--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -1,20 +1,4 @@
 <template>
-<<<<<<< HEAD
-
-      <div>
-       <CardTeam />
-      </div>
-    <SideBar class="col-2"/>
-    <div class="col-10 px-5" >
-      <h1>"esto es h1 en el About"</h1>
-      <h2>"Esto es h2"</h2>
-      <h3>"esto es h3"</h3>
-      <p>"esto es un parrafo"</p>
-
-    </div>
- 
-    
-=======
   
   <SideBar class=" col-lg-2  col-md-3"/>
   <div class="col-lg-10 col-md-9" >
@@ -22,24 +6,12 @@
     <FormContact/>
 
   </div>
->>>>>>> daa27a38b839d754d46af2d2e402f67d732b774b
-
-
-<<<<<<< HEAD
-<script setup>
-  import SideBar from '@/components/Commons/SideBar.vue'
-  
-   import CardTeam from '@/components/Cards/CardTeam.vue'
-
-
-=======
 
 </template>
 
 <script setup>
 import SideBar from '@/components/Commons/SideBar.vue'
 import FormContact from '@/components/Forms/FormContact.vue';
->>>>>>> daa27a38b839d754d46af2d2e402f67d732b774b
 
 </script>
 
