2b6ebb950996c0eb6a84c98b3e46a4a206aaa8e6
nasre21 <nasou2001@gmail.com>
2023-03-29 13:34:09 +0200

    terminar test unitario

--- tests/unit/Forms/FormContact.spec.js
+++ tests/unit/Forms/FormContact.spec.js
@@ -4,8 +4,7 @@ describe("test FormContact",()=>{
     it("test formulario",()=>{
     const msg = '<a href="#" class="btn btn-primary">Send Message</a>'
     const wrapper = mount(FormContact)
-    expect(wrapper.html()).toMatch(msg)
-    
+    expect(wrapper.html()).toMatch(msg) 
 })
 it("testar un unitario",()=>{
     const msg = ' <p class="card-text"> If you want to contact us you can do it with this contact form </p>'
@@ -13,4 +12,19 @@ it("testar un unitario",()=>{
     expect(wrapper.html()).toMatch(msg)
     
 })
+it("testear un href",()=>{
+   
+    const wrapper = mount(FormContact)
+    const msg = wrapper.get("a")
+    console.log(msg)
+    expect(wrapper.html()).toMatch("Send Message")
+})
+it("testear un parrafo",()=>{
+   
+    const wrapper = mount(FormContact)
+    const msg = wrapper.get("p")
+    console.log(msg)
+    expect(wrapper.html()).toMatch("If you want to contact us you can do it with this contact form")
+})
+
 })
