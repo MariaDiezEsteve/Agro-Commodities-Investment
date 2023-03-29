48e39c76b8fe7713dede918080b94924e35d8742
nasre21 <nasou2001@gmail.com>
2023-03-29 14:05:06 +0200

    resolver con vue

--- tests/unit/Forms/FormContact.spec.js
+++ tests/unit/Forms/FormContact.spec.js
@@ -1,25 +1,25 @@
 import { mount } from '@vue/test-utils'
 import FormContact from '@/components/Forms/FormContact.vue'
 describe("test FormContact",()=>{
-    it("test formulario",()=>{
+    it("test form",()=>{
     const msg = '<a href="#" class="btn btn-primary">Send Message</a>'
     const wrapper = mount(FormContact)
     expect(wrapper.html()).toMatch(msg) 
 })
-it("testar un unitario",()=>{
+it("test unit",()=>{
     const msg = ' <p class="card-text"> If you want to contact us you can do it with this contact form </p>'
     const wrapper = mount(FormContact)
     expect(wrapper.html()).toMatch(msg)
     
 })
-it("testear un href",()=>{
+it("test  href",()=>{
    
     const wrapper = mount(FormContact)
     const msg = wrapper.get("a")
     console.log(msg)
     expect(wrapper.html()).toMatch("Send Message")
 })
-it("testear un parrafo",()=>{
+it("test a p",()=>{
    
     const wrapper = mount(FormContact)
     const msg = wrapper.get("p")
