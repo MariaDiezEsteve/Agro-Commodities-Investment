6aad4c0863953549ec9633888b88940a01403a36
MariaDiezEsteve <diezesteve@outlook.es>
2023-03-29 11:53:11 +0200

    Solucionado el problema de renderizar las reviews cuando se crean. creada la ruta de borrado y actualizada la las reviews cuando se hace. Falta los estilos del formulario, y falta la parte de la ruta de actualización

--- src/components/Forms/FormReview.vue
+++ src/components/Forms/FormReview.vue
@@ -1,31 +1,47 @@
 <template>
   <div class="reviews">
     <h1>Reviews</h1>
-    <div class="card text-center" v-for="review in reviews.getReviews" :key="review.id" >
-      <div class="card-body">
-        <h3 class="card-title"> {{ review.name }}</h3>
-        <p class="opinion">Review:</p>
-        <p class="card-text"> {{ review.opinion }}</p>
-        <button>Edit Review</button>
-        <button>Delete Review</button>
-      </div>
-    </div>   
+    <img v-if="reviewsData.isError" src="@/assets/images/error.jpeg" alt="error">
+        <div v-if="!reviewsData.isError" >
+          <div class="card text-center" v-for="review in reviewsData.getReviews" :key="review.id" >
+            <div class="card-body">
+              <h3 class="card-title"> {{ review.name }}</h3>
+              <p class="opinion">Review:</p>
+              <p class="card-text"> {{ review.opinion }}</p>
+              <button>Edit Review</button>
+              <button @click="deleteReview(review.id)">Delete Review</button>
+            </div>
+          </div>
+        </div>   
   </div>
 </template>
   
   <script setup>
   import {defineProps } from 'vue'
-
+  import axios from 'axios'
   
-  defineProps({
-    reviews: Object
+ const prop = defineProps({
+    reviewsData: Object,
+    reviews: Function
   });
 
 
 
-  // EDIT REVIEW
-
-
+  // DELETE REVIEW
+  let isError = false
+  const deleteReview = (idReview) => {
+        try{
+            axios.delete(`http://localhost:3000/reviews/${idReview}`)  
+            // console.log("id será", idReview)
+             prop.reviews()
+          }catch (error) {
+              console.log(error);
+              isError = true
+          }
+    return{
+        isError
+    } 
+  }
 
 
   </script>
