faef1e35d3b6d544365c941d37d31ffdd94d54fb
nasre21 <nasou2001@gmail.com>
2023-03-31 14:18:42 +0200

    seguir el trabajo  de tabla

--- src/components/Charts/TableChart.vue
+++ src/components/Charts/TableChart.vue
@@ -1,13 +1,32 @@
 <template>
-    <h1></h1>
+  
+ <table>
+      <thead>
+        <tr>
+        <th v-for="month in months" :key="month"> {{ month }}</th>
+      </tr>
+        <tr v-for="year in years" :key="year">
+          <td >{{year}}</td>
+        </tr>
+      </thead>
+    </table>
   </template>
   
   <script setup>
+   import {defineProps} from 'vue';
+    defineProps({
+    data: Object
+    })
+
+
+   const months = ["Prices","Jan","Feb","Mar","Abr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+  const years = [2022, 2021, 2020, 2019, 2018]
 
 </script>
   
   <style lang="scss" scoped>
     @import "@/assets/Sass/--parcial.scss";
 
+    
   
   </style>
