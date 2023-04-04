feb5bc08c2bb01a10b7ea348514b4283c7402d72
Zguedest <zucelys.zge@gmail.com>
2023-04-04 09:46:20 +0200

    responsive contact form

--- src/components/Forms/FormContact.vue
+++ src/components/Forms/FormContact.vue
@@ -1,36 +1,34 @@
 <template>
-  <div class="black" @submit.prevent="onSubmit">
-     <div class="form-card " >
+  <div class="black">
+     <div class="form-card" >
        <div class="formTitle card-body">
          <h5 class="card-title">Contact Form</h5>
          <p class="card-text">
-           If you want to contact us you can do it with this contact form
+           If you want to contact us you can do it with this contact form:
          </p>
        </div>
  
        <div class="cuadro row">
-         <div class=" mt-4 mr-1 col-6 px-5">
-           <input 
-             v-model="name"
+         <div class="inputDiv row ">
+         <div class="input1 mt-4 mr-1 col-lg-6 col-md-6 ps-4">
+           <input
              type="text"
              class="form-control p-2"
              placeholder="Enter your name"
            />
          </div>
-         <div class="  mr-1 mt-4 col-5">
+         <div class="  mr-1 mt-4 col-lg-6 col-md-6 px-3">
            <input
-           v-model="email"
- 
              type="text"
              class="form-control p-2"
              placeholder="Enter your email address"
            />
          </div>
+       </div>
  
-         <div class="mb-4 ps-5 col-11">
+         <div class="formTextDiv mb-4 ps-5 col-lg-11">
            <label for="exampleFormControlTextarea1" class="form-label"></label>
            <textarea
-           v-model="msg"
              class="form-control"
              placeholder="Type here your message"
              id="exampleFormControlTextarea1"
@@ -44,29 +42,22 @@
        </div>
      </div>
    </div>
- 
-   <pre>
-     nombre: {{ name }}
-   </pre>
   
    <!-- FORMULARIO -->
  </template>
  
-  <script setup>
-   import { ref } from "vue";
-  const name = ref("")
-  const email = ref("")
-  const msg = ref("")
- 
- //  const onSubmit= ()=> {
- //   const form = {
- //     name: name.value,
- //     message:msg.value,
- //     email:email.value
- //   }
- //  }
- 
- </script> 
+ <!-- <script setup>
+ const formContact = [
+   {
+     id: 1,
+     title: "Contact Form",
+     name: "",
+     email: "",
+     text: "",
+     button: "Send Message",
+   },
+ ];
+ </script> -->
  
  
    
@@ -97,12 +88,47 @@
      text-align: center;
      margin-top: 3rem;
     }
- 
-    .form-card {
-     justify-self: center;
+    
+    .card-body >p {
+     margin-bottom: 1.2rem;
     }
  
+    .card-title {
+     font-weight: bold;
+    }
  
-       </style>
+ @include media-breakpoint-down(sm) {
+   .formDiv {
+   width: 18rem;
+   //margin: 0 auto; 
+ }
+ 
+ .inputDiv {
+   display: block;
+ }
+ 
+ .input1 {
+   margin-left: -0.8rem;
+ }
+ .formTextDiv {
+   
+   margin-left: -1.7rem;
+ 
+ }
+ }
+ @include media-breakpoint-down(lg) {
+   .input1 {
+   width: 17.5rem;
+   margin-left: -0.5rem;
+ }
+ .formTextDiv {
+   
+   margin-left: -1.5rem;
+ 
+ }
+ 
+ }
+ 
+</style>
  
  
