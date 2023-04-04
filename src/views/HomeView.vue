45020fbe65101c00d5e1ad7e379d3aba01309516
gorka1999 <gorkaibarrondo23@gmail.com>
2023-04-04 12:06:15 +0200

    confictos fixed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@
       <ImgSlider/>
       <div class="boxContainer">
         <h2 class="pt-3 pb-3">Price interaction across all products</h2>
-        <img v-if="data.isError" src="@/assets/images/error.jpeg" alt="error">
+        <img v-if="data.isError" src="@/assets/images/error.gif" alt="error">
         <img v-if="isLoading" src="@/assets/images/spin.gif" alt="loading">
         <div v-if="!data.isError && !isLoading" >
           <InterectChart :data="data" :vista="vista"/>
